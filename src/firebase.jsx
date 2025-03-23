76162a2830cba3585e59c25db755d3f5dd3eac6c
tanmay-37 <talekartk@rknec.edu>
2025-03-23 14:57:54 +0530

    removed api keys

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -7,13 +7,16 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDaShCD5ztywDaChVL_0Mk_qIyY_yExPt0",
-  authDomain: "eventease-auth.firebaseapp.com",
-  projectId: "eventease-auth",
-  storageBucket: "eventease-auth.firebasestorage.app",
-  messagingSenderId: "920430522537",
-  appId: "1:920430522537:web:a1ccded332fae062d7b9b8"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
